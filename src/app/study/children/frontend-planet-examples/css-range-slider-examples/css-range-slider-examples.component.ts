b2c7d001e6a8fbc000c398a18bc3a1a6f373be99
asdflkj169 <asdflkj169@gmail.com>
2024-11-16 12:20:13 +0900

    add RangeSliderEqualizerComponent

--- src/app/study/children/frontend-planet-examples/css-range-slider-examples/css-range-slider-examples.component.ts
+++ src/app/study/children/frontend-planet-examples/css-range-slider-examples/css-range-slider-examples.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 
+import { RangeSliderEqualizerComponent } from './range-slider-equalizer/range-slider-equalizer.component';
+
 @Component({
   selector: 'app-css-range-slider-examples',
   standalone: true,
-  imports: [],
+  imports: [RangeSliderEqualizerComponent],
   templateUrl: './css-range-slider-examples.component.html',
   styleUrl: './css-range-slider-examples.component.scss',
 })
