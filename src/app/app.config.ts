0dd997f1f143498f5fe64503a334c959bf6257ec
asdflkj169 <asdflkj169@gmail.com>
2025-02-01 00:24:19 +0900

    お試し

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,17 @@
 import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 
+import { firebaseConfig } from '../environments/environment.development';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
     provideRouter(
       routes,
       withInMemoryScrolling({
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,13 @@
 export const environment = {
   articleRootUrl: 'http://localhost:4200/',
 };
+
+export const firebaseConfig = {
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: '',
+};
