7378029480a0ac58a19d11cd26eef527787e212c
asdflkj169 <asdflkj169@gmail.com>
2025-02-01 03:21:18 +0900

    確認できたから削除

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,16 +5,6 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 import { Contents } from './shared/contents';
 import { CardModule } from 'primeng/card';
 
-import {
-  FIREBASECONFIG_APIKEY,
-  FIREBASECONFIG_APPID,
-  FIREBASECONFIG_AUTHDOMAIN,
-  FIREBASECONFIG_MEASUREMENTID,
-  FIREBASECONFIG_MESSAGINGSENDERID,
-  FIREBASECONFIG_PROJECTID,
-  FIREBASECONFIG_STORAGEBUCKET,
-} from '../environments/environment';
-
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -27,13 +17,4 @@ export class AppComponent {
   contents = Contents;
   protected readonly Object = Object;
   a = inject(Analytics);
-  constructor() {
-    console.log(FIREBASECONFIG_APPID);
-    console.log(FIREBASECONFIG_APIKEY);
-    console.log(FIREBASECONFIG_AUTHDOMAIN);
-    console.log(FIREBASECONFIG_PROJECTID);
-    console.log(FIREBASECONFIG_MESSAGINGSENDERID);
-    console.log(FIREBASECONFIG_MEASUREMENTID);
-    console.log(FIREBASECONFIG_STORAGEBUCKET);
-  }
 }
