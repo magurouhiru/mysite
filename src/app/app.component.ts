50f5bb007d2c9b06459ee2d5002b58ef0ebaf90c
asdflkj169 <asdflkj169@gmail.com>
2025-02-01 03:17:11 +0900

    念のためコンソールでも確認

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,16 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 import { Contents } from './shared/contents';
 import { CardModule } from 'primeng/card';
 
+import {
+  FIREBASECONFIG_APIKEY,
+  FIREBASECONFIG_APPID,
+  FIREBASECONFIG_AUTHDOMAIN,
+  FIREBASECONFIG_MEASUREMENTID,
+  FIREBASECONFIG_MESSAGINGSENDERID,
+  FIREBASECONFIG_PROJECTID,
+  FIREBASECONFIG_STORAGEBUCKET,
+} from '../environments/environment';
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -17,4 +27,13 @@ export class AppComponent {
   contents = Contents;
   protected readonly Object = Object;
   a = inject(Analytics);
+  constructor() {
+    console.log(FIREBASECONFIG_APPID);
+    console.log(FIREBASECONFIG_APIKEY);
+    console.log(FIREBASECONFIG_AUTHDOMAIN);
+    console.log(FIREBASECONFIG_PROJECTID);
+    console.log(FIREBASECONFIG_MESSAGINGSENDERID);
+    console.log(FIREBASECONFIG_MEASUREMENTID);
+    console.log(FIREBASECONFIG_STORAGEBUCKET);
+  }
 }
