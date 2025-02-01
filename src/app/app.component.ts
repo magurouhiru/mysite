5d9643bd7cc960777f4f201911d53265c3e142bd
asdflkj169 <asdflkj169@gmail.com>
2025-02-01 17:56:26 +0900

    これだとページごとの視聴回数が出るはず？

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Analytics, ScreenTrackingService } from '@angular/fire/analytics';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 import { Contents } from './shared/contents';
@@ -10,10 +11,12 @@ import { CardModule } from 'primeng/card';
   imports: [RouterOutlet, RouterLink, CardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  providers: [],
+  providers: [ScreenTrackingService],
 })
 export class AppComponent {
   title = 'mysite';
   contents = Contents;
   protected readonly Object = Object;
+  s = inject(ScreenTrackingService);
+  a = inject(Analytics);
 }
