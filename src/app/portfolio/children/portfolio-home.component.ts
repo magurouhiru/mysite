a92361d4a6b0ffc62520bec19c073f9c055582e7
asdflkj169 <asdflkj169@gmail.com>
2025-02-07 20:56:38 +0900

    use serverTimestamp

--- src/app/portfolio/children/portfolio-home.component.ts
+++ src/app/portfolio/children/portfolio-home.component.ts
@@ -2,7 +2,9 @@ import { AsyncPipe, NgOptimizedImage } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import {
+  FieldValue,
   Timestamp,
+  serverTimestamp,
   Firestore,
   FirestoreDataConverter,
   QueryDocumentSnapshot,
@@ -11,7 +13,7 @@ import {
 } from '@angular/fire/firestore';
 import { getDownloadURL, ref, Storage } from '@angular/fire/storage';
 import { RouterLink } from '@angular/router';
-import { from, map, NEVER, Observable } from 'rxjs';
+import { from, map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-portfolio-home',
@@ -29,75 +31,60 @@ export class PortfolioHomeComponent {
     'portfolio/home/portfolio_info',
   ).withConverter(portfolioInfoConverter);
   readonly portfolioInfos = toSignal(
-    collectionData<PortfolioInfo>(this.#portfolioInfoCollection).pipe(
+    collectionData<PortfolioInfoApp>(this.#portfolioInfoCollection).pipe(
       map((infos) => {
         infos.forEach((info) => {
-          info.setThumbnailUrl(
-            from(getDownloadURL(ref(this.storage, info.thumbnail_path))),
+          info.thumbnail_url = from(
+            getDownloadURL(ref(this.storage, info.thumbnail_path)),
           );
-          console.log(info.create_at);
         });
-        console.log(Timestamp.now());
-        return infos.sort(
-          (a, b) => b.create_at.toMillis() - a.create_at.toMillis(),
-        );
+        infos.sort((a, b) => b.created_at.toMillis() - a.created_at.toMillis());
+        return infos;
       }),
     ),
   );
 }
 
-class PortfolioInfo {
-  thumbnail_url: Observable<string> = NEVER;
+interface PortfolioInfoBody {
+  label: string;
+  thumbnail_path: string;
+  reference_url: string;
+  portfolio_link: string;
+  concept: string;
+  plan: string;
+  specification: string;
+  reflection: string;
+}
 
-  constructor(
-    readonly label: string,
-    readonly thumbnail_path: string,
-    readonly reference_url: string,
-    readonly portfolio_link: string,
-    readonly concept: string,
-    readonly plan: string,
-    readonly specification: string,
-    readonly reflection: string,
-    readonly create_at: Timestamp,
-  ) {
-    this.label = label;
-    this.thumbnail_path = thumbnail_path;
-    this.reference_url = reference_url;
-    this.portfolio_link = portfolio_link;
-    this.concept = concept;
-    this.plan = plan;
-    this.specification = specification;
-    this.reflection = reflection;
-    this.create_at = create_at;
-  }
+interface PortfolioInfoApp extends PortfolioInfoBody {
+  created_at: Timestamp;
+  thumbnail_url?: Observable<string>;
+}
 
-  setCreateAtNow() {
-    // this.create_at = Timestamp.now();
-  }
-  setThumbnailUrl(thumbnail_url: Observable<string>) {
-    this.thumbnail_url = thumbnail_url;
-  }
+interface PortfolioInfoDb extends PortfolioInfoBody {
+  created_at: FieldValue;
 }
 
 const portfolioInfoConverter: FirestoreDataConverter<
-  PortfolioInfo,
-  PortfolioInfo
+  PortfolioInfoApp,
+  PortfolioInfoDb
 > = {
-  toFirestore(portfolioInfo: PortfolioInfo) {
-    return portfolioInfo;
+  toFirestore(data) {
+    return {
+      label: data.label,
+      thumbnail_path: data.thumbnail_path,
+      reference_url: data.reference_url,
+      portfolio_link: data.portfolio_link,
+      concept: data.concept,
+      plan: data.plan,
+      specification: data.specification,
+      reflection: data.reflection,
+      created_at: serverTimestamp(),
+    } as PortfolioInfoDb;
   },
-  fromFirestore(snapshot: QueryDocumentSnapshot<PortfolioInfo, PortfolioInfo>) {
-    const data = snapshot.data();
-    return new PortfolioInfo(
-      data.label,
-      data.thumbnail_path,
-      data.reference_url,
-      data.portfolio_link,
-      data.concept,
-      data.plan,
-      data.specification,
-      data.reflection,
-      data.create_at,
-    );
+  fromFirestore(
+    snapshot: QueryDocumentSnapshot<PortfolioInfoApp, PortfolioInfoDb>,
+  ) {
+    return snapshot.data();
   },
 };
